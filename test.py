5691ae7fe11cac5ba61129cb9b1316065a2abd1b
Gurbax Barhe <gurbax.barhe@gmail.com>
2024-03-04 13:55:26 -0500

    Added encoder logic, moving away from time-based approach for joint movement

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 from roboclaw import Roboclaw
 from serial import Serial
 import time
+import serial.tools.list_ports
 
-motor_com_port = 'COM18'
+import serial.tools.list_ports
+
+def find_motor_com_port(unique_identifier):
+    ports = serial.tools.list_ports.comports()
+    for port, desc, hwid in ports:
+        if unique_identifier.lower() in hwid.lower():
+            return port
+    return None
+
+if __name__ == "__main__":
+    motor_com_port = find_motor_com_port("roboclaw_controller")
+    if motor_com_port:
+        print(f"Motor COM port found: {motor_com_port}")
+    else:
+        print("Motor COM port not found.")
+
+#motor_com_port = 'COM18'
 
 serial_obj = Serial(motor_com_port, 38400) # default baudrate is 38400
 rc = Roboclaw(serial_obj)
