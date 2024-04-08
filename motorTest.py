413c8878d506f577e57d221a55829a32f0212cb6
Gurbax Barhe <barheg@mcmaster.ca>
2024-04-08 16:53:56 -0400

    Added stepper code for height mechanism

---
+++ motorTest.py
@@ -0,0 +1,43 @@
+from Phidget22.Devices.BLDCMotor import *
+from Phidget22.Phidget import *
+import time
+def onAttach(self):
+    print("Attach!")
+def onDetach(self):
+    print("Detach!")
+
+elbowMotor = BLDCMotor()
+shoulderMotor = BLDCMotor()
+
+elbowMotor.setDeviceSerialNumber(708023)
+shoulderMotor.setDeviceSerialNumber(708023)
+elbowMotor.setHubPort(4)
+shoulderMotor.setHubPort(3)
+
+elbowMotor.setOnAttachHandler(onAttach)
+shoulderMotor.setOnAttachHandler(onAttach)
+elbowMotor.setOnDetachHandler(onDetach)
+shoulderMotor.setOnDetachHandler(onDetach)
+
+elbowMotor.openWaitForAttachment(1000)
+elbowMotor.setTargetVelocity(-0.5)
+time.sleep(2)
+elbowMotor.close()
+
+shoulderMotor.openWaitForAttachment(1000)
+shoulderMotor.setTargetVelocity(0.5)
+time.sleep(2)
+shoulderMotor.close()
+
+elbowMotor.openWaitForAttachment(1000)
+elbowMotor.setTargetVelocity(-0.5)
+time.sleep(2)
+elbowMotor.close()
+
+
+
+
+#try:
+#	input("Press Enter to Stop\n")
+#except (Exception, KeyboardInterrupt):
+#	pass
--- test.py
+++ test.py
@@ -1,60 +1,20 @@
-from roboclaw import Roboclaw
-from serial import Serial
+from Phidget22.Phidget import *
+from Phidget22.Devices.Stepper import *
 import time
-import serial.tools.list_ports
 
-import serial.tools.list_ports
+def main():
+	stepper0 = Stepper()
 
-def find_motor_com_port(unique_identifier):
-    ports = serial.tools.list_ports.comports()
-    for port, desc, hwid in ports:
-        if unique_identifier.lower() in hwid.lower():
-            return port
-    return None
+	stepper0.openWaitForAttachment(5000)
 
-if __name__ == "__main__":
-    motor_com_port = find_motor_com_port("roboclaw_controller")
-    if motor_com_port:
-        print(f"Motor COM port found: {motor_com_port}")
-    else:
-        print("Motor COM port not found.")
+	stepper0.setTargetPosition(50000)
+	stepper0.setEngaged(True)
 
-#motor_com_port = 'COM18'
+	try:
+		input("Press Enter to Stop\n")
+	except (Exception, KeyboardInterrupt):
+		pass
 
-serial_obj = Serial(motor_com_port, 38400) # default baudrate is 38400
-rc = Roboclaw(serial_obj)
-rc.forward_backward_mixed(64) # stops both motors
+	stepper0.close()
 
-timing = 10
-power_rating = 32
-stop = 0
-
-while 1:
-    rc.forward_m1((power_rating))  # 1/4 power forward
-    rc.backward_m2((power_rating))  # 1/4 power backward
-    time.sleep((timing))
-
-    rc.backward_m1((power_rating))  # 1/4 power backward
-    time.sleep((timing))
-
-    rc.backward_m1((stop))  # Stopped
-    rc.forward_m2((stop))  # Stopped
-    time.sleep((timing))
-
-    m1duty = 16
-    m2duty = -16
-    rc.forward_backward_m1(64+m1duty)  # 1/4 power forward
-    rc.forward_backward_m2(64+m2duty)  # 1/4 power backward
-    time.sleep((timing))
-
-    m1duty = -16
-    m2duty = 16
-    rc.forward_backward_m1(64+m1duty)  # 1/4 power backward
-    rc.forward_backward_m2(64+m2duty)  # 1/4 power forward
-    time.sleep((timing))
-
-    rc.forward_backward_m1(64)  # Stopped
-    rc.forward_backward_m2(64)  # Stopped
-    time.sleep((timing))
-
-
+main()
